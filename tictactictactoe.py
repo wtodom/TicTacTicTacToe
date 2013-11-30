bdf68123a725f511cee83917bf95739962372cec
Weston Odom <weston.odom@gmail.com>
2013-11-30 09:00:45 -0600

    Converted to class, finished y, x translation function to automatically place cursor in the correct location after marking your X or O.

--- tictactictactoe.py
+++ tictactictactoe.py
@@ -1,70 +1,106 @@
 import curses
 
+class TicTacTicTacToe:
 
-marks = ["-", "X", "O"]
-player = 1  # this will be used as the index of the above list to place marks
-HEIGHT = 12
-WIDTH = 24
+	def __init__(self):
+		self.q1 = [[5,2], [5,10], [5,18], [9,2], [9,10], [9,18], [13,2], [13,10], [13,18]]
+		self.q2 = [[5,4], [5,12], [5,20], [9,4], [9,12], [9,20], [13,4], [13,12], [13,20]]
+		self.q3 = [[5,6], [5,14], [5,22], [9,6], [9,14], [9,22], [13,6], [13,14], [13,22]]
+		self.q4 = [[6,2], [6,10], [6,18], [10,2], [10,10], [10,18], [14,2], [14,10], [14,18]]
+		self.q5 = [[6,4], [6,12], [6,20], [10,4], [10,12], [10,20], [14,4], [14,12], [14,20]]
+		self.q6 = [[6,6], [6,14], [6,22], [10,6], [10,14], [10,22], [14,6], [14,14], [14,22]]
+		self.q7 = [[7,2], [7,10], [7,18], [11,2], [11,10], [11,18], [15,2], [15,10], [15,18]]
+		self.q8 = [[7,4], [7,12], [7,20], [11,4], [11,12], [11,20], [15,4], [15,12], [15,20]]
+		self.q9 = [[7,6], [7,14], [7,22], [11,6], [11,14], [11,22], [15,6], [15,14], [15,22]]
 
-# default these to a zero.
-# change to appropriate player value (from above) when noventile is won.
-nontiles = [[0, 0, 0],
-			[0, 0, 0],
-			[0, 0, 0]]
+		self.marks = ["X", "O"]
+		self.player = 0  # this will be used as the index of the above list to place marks
 
+		# default these to a zero.
+		# change to appropriate player value (from above) when noventile is won.
+		self.captured_nontiles = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-def main(stdscr):
-	# Clear screen
-	stdscr.clear()
+	def main(self, stdscr):
+		# Clear screen
+		stdscr.clear()
 
-	setup(stdscr)
+		self.setup(stdscr)
 
-	x = 2
-	y = 5
-	stdscr.move(y, x)
+		x = 2
+		y = 5
+		stdscr.move(y, x)
 
-	while True:
-		key = stdscr.getkey()
-		if key == "x":
-			break
-		elif key == "r":
-			x = 2
-			y = 5
-			stdscr.move(y, x)
-		elif key == "KEY_UP":
-			if y > 5:
-				y -= 1
-				if y == 8 or y == 12:
+		while True:
+			key = stdscr.getkey()
+			if key == "x":
+				break
+			elif key == "r":
+				x = 2
+				y = 5
+				stdscr.move(y, x)
+			elif key == "KEY_UP":
+				if y > 5:
 					y -= 1
-				stdscr.move(y, x)
-				stdscr.refresh()
-		elif key == "KEY_DOWN":
-			if y < 15:
-				y += 1
-				if y == 8 or y == 12:
+					if y == 8 or y == 12:
+						y -= 1
+					stdscr.move(y, x)
+					stdscr.refresh()
+			elif key == "KEY_DOWN":
+				if y < 15:
 					y += 1
-				stdscr.move(y, x)
-				stdscr.refresh()
-		elif key == "KEY_LEFT":
-			if x > 2:
-				x -= 2
-				if x == 8 or x == 16:
+					if y == 8 or y == 12:
+						y += 1
+					stdscr.move(y, x)
+					stdscr.refresh()
+			elif key == "KEY_LEFT":
+				if x > 2:
 					x -= 2
-				stdscr.move(y, x)
-				stdscr.refresh()
-		elif key == "KEY_RIGHT":
-			if x < 22:
-				x += 2
-				if x == 8 or x == 16:
+					if x == 8 or x == 16:
+						x -= 2
+					stdscr.move(y, x)
+					stdscr.refresh()
+			elif key == "KEY_RIGHT":
+				if x < 22:
 					x += 2
-				stdscr.move(y, x)
-				stdscr.refresh()
-		else:
-			stdscr.addstr(key)
+					if x == 8 or x == 16:
+						x += 2
+					stdscr.move(y, x)
+					stdscr.refresh()
+			elif key == " ":
+				if stdscr.instr(1) == b"-":
+					stdscr.addstr(self.marks[self.player])
+					self.player = not self.player
+					y, x = self.translate(y, x)
+					stdscr.move(y, x)
+					stdscr.refresh()
+				else:
+					pass
+			else:
+				stdscr.addstr(key)
 
+	def translate(self, y, x):
+		loc = [y, x]
+		if loc in self.q1:
+			return 6, 4
+		elif loc in self.q2:
+			return 6, 12
+		elif loc in self.q3:
+			return 6, 20
+		elif loc in self.q4:
+			return 10, 4
+		elif loc in self.q5:
+			return 10, 12
+		elif loc in self.q6:
+			return 10, 20
+		elif loc in self.q7:
+			return 14, 4
+		elif loc in self.q8:
+			return 14, 12
+		else:  # if loc in q9:
+			return 14, 20
 
-def setup(stdscr):
-	grid ="""
+	def setup(self, stdscr):
+		grid ="""
 ┌-------┬-------┬-------┐
 | - - - | - - - | - - - |
 | - - - | - - - | - - - |
@@ -77,91 +113,21 @@ def setup(stdscr):
 | - - - | - - - | - - - |
 | - - - | - - - | - - - |
 | - - - | - - - | - - - |
-└-------┴-------┴-------┘
-"""
+└-------┴-------┴-------┘"""
 
-	x = y = 0
+		x = y = 0
 
-	stdscr.addstr(y, x, "Use the arrow keys to move the cursor.")
-	y += 1
-	stdscr.addstr(y, x, "Press the spacebar to place your mark.")
-	y += 1
-	stdscr.addstr(y, x, "Press 'r' to reset the game.")
-	y += 1
-	stdscr.addstr(y, x, "Press 'x' to quit.")
-	y += 1
-	stdscr.addstr(grid)
-	# horiz_border = "-------"
-	# x = y = 0
-
-	# def full_row(y, x, l, m, r):
-	# 	stdscr.addch(y, x, l)
-	# 	x += 1
-	# 	stdscr.addstr(y, x, horiz_border)
-	# 	x += len(horiz_border)
-	# 	stdscr.addch(y, x, m)
-	# 	x += 1
-	# 	stdscr.addstr(y, x, horiz_border)
-	# 	x += len(horiz_border)
-	# 	stdscr.addch(y, x, m)
-	# 	x += 1
-	# 	stdscr.addstr(y, x, horiz_border)
-	# 	x += len(horiz_border)
-	# 	stdscr.addch(y, x, r)
-	# 	y += 1
-	# 	x = 0
-	# 	return y, x
-
-	# def empty_row(y, x, l, m, r):
-	# 	stdscr.addch(y, x, l)
-	# 	x += 1
-	# 	stdscr.addstr(y, x, " " * len(horiz_border))
-	# 	x += len(horiz_border)
-	# 	stdscr.addch(y, x, m)
-	# 	x += 1
-	# 	stdscr.addstr(y, x, " " * len(horiz_border))
-	# 	x += len(horiz_border)
-	# 	stdscr.addch(y, x, m)
-	# 	x += 1
-	# 	stdscr.addstr(y, x, " " * len(horiz_border))
-	# 	x += len(horiz_border)
-	# 	stdscr.addch(y, x, r)
-	# 	y += 1
-	# 	x = 0
-	# 	return y, x
+		stdscr.addstr(y, x, "Use the arrow keys to move the cursor.")
+		y += 1
+		stdscr.addstr(y, x, "Press the spacebar to place your mark.")
+		y += 1
+		stdscr.addstr(y, x, "Press 'r' to reset the game.")
+		y += 1
+		stdscr.addstr(y, x, "Press 'x' to quit.")
+		y += 1
+		stdscr.addstr(grid)
 
 
-	# stdscr.addstr(y, x, "Use the arrow keys to move the cursor.")
-	# y += 1
-	# stdscr.addstr(y, x, "Press the spacebar to place your mark.")
-	# y += 1
-	# stdscr.addstr(y, x, "Press 'r' to reset the game.")
-	# y += 1
-	# stdscr.addstr(y, x, "Press 'x' to quit.")
-	# y += 1
-
-	# # top line of grid
-	# y, x = full_row(y, x, curses.ACS_ULCORNER, curses.ACS_TTEE, curses.ACS_URCORNER)
-
-	# for i in range(3):
-	# 	y, x = empty_row(y, x, curses.ACS_VLINE, curses.ACS_VLINE, curses.ACS_VLINE)
-
-	# # lines for the first row of boards
-	# y, x = full_row(y, x, curses.ACS_LTEE, curses.ACS_VLINE, curses.ACS_RTEE)
-
-	# # empty rows between 1 and 2
-	# for i in range(3):
-	# 	y, x = empty_row(y, x, curses.ACS_VLINE, curses.ACS_VLINE, curses.ACS_VLINE)
-
-	# # lines for row 2
-	# y, x = full_row(y, x, curses.ACS_LTEE, curses.ACS_VLINE, curses.ACS_RTEE)
-
-	# # empty rows between 2 and 3
-	# for i in range(3):
-	# 	y, x = empty_row(y, x, curses.ACS_VLINE, curses.ACS_VLINE, curses.ACS_VLINE)
-
-	# # bottom row of grid
-	# y, x = full_row(y, x, curses.ACS_LLCORNER, curses.ACS_BTEE, curses.ACS_LRCORNER)
-
-
-curses.wrapper(main)
+if __name__ == '__main__':
+	game = TicTacTicTacToe()
+	curses.wrapper(game.main)
